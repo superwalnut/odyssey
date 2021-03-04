efce743a5e3f91e2e4eb93f5643350d7b09464f9
vikingbei <bei2004@gmail.com>
2021-03-05 00:18:39 +1100

    sitesetting-shuttle form validator

---
+++ src/app/models/shuttle.ts
@@ -0,0 +1,7 @@
+export class Shuttle {
+  docId: string;
+  purchaseDate: Date;
+  cost: number;
+  quantity: number;
+  notes: string;
+}
--- src/app/views/admin/sitesettings/sitesettings.component.ts
+++ src/app/views/admin/sitesettings/sitesettings.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl } from "@angular/forms";
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from "@angular/forms";
+
+import { Shuttle } from "../../../models/shuttle";
+//import { Shuttle } from "@models/shuttle";
 
 @Component({
   selector: "app-sitesettings",
@@ -7,12 +15,42 @@ import { FormGroup, FormControl } from "@angular/forms";
   styleUrls: ["./sitesettings.component.scss"],
 })
 export class SitesettingsComponent implements OnInit {
+  shuttleForm: FormGroup;
+  shuttleSubmitted = false;
   range = new FormGroup({
     start: new FormControl(),
     end: new FormControl(),
   });
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.shuttleForm = this.fb.group({
+      purchaseDate: ["", Validators.required],
+      cost: ["", Validators.required],
+      quantity: ["", Validators.required],
+      notes: ["", Validators.required],
+    });
+  }
+
+  onShuttleSubmit() {
+    this.shuttleSubmitted = true;
+
+    var shuttle = {
+      purchaseDate: this.shuttleForm.value.purchaseDate,
+      cost: this.shuttleForm.value.cost,
+      quantity: this.shuttleForm.value.quantity,
+      notes: this.shuttleForm.value.notes,
+    } as Shuttle;
+    console.log(shuttle);
+  }
+  get sf() {
+    return this.shuttleForm.controls;
+  }
 }
+
+// docId: string;
+// purchaseDate: Date;
+// cost: number;
+// quantity: number;
+// notes: string;
