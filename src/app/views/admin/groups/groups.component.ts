bee7662e7a1000f71f3eda92a584637d573bbfb0
vikingbei <bei2004@gmail.com>
2021-03-07 12:26:52 +1100

    wired up groups to db

--- src/app/views/admin/groups/groups.component.ts
+++ src/app/views/admin/groups/groups.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from "@angular/core";
+import { Group } from "../../../models/group";
+import { MatSort } from "@angular/material/sort";
+import { GroupService } from "../../../services/group.service";
 
+import { MatTableDataSource } from "@angular/material/table";
 @Component({
-  selector: 'app-groups',
-  templateUrl: './groups.component.html',
-  styleUrls: ['./groups.component.scss']
+  selector: "app-groups",
+  templateUrl: "./groups.component.html",
+  styleUrls: ["./groups.component.scss"],
 })
 export class GroupsComponent implements OnInit {
+  groups: Group[] = [];
+  displayedColumns: string[] = [
+    "startDate",
+    "endDate",
+    "termCost",
+    "groupName",
+    "groupDesc",
+    "isClosed",
+  ];
+  dataSource = new MatTableDataSource<Group>();
+  @ViewChild(MatSort) sort: MatSort;
 
-  constructor() { }
+  constructor(private groupService: GroupService) {}
 
   ngOnInit(): void {
+    this.getAllGroups();
+  }
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
   }
 
+  getAllGroups() {
+    this.groupService.getGroups().subscribe((x) => {
+      console.log(x);
+      this.dataSource.data = x;
+    });
+  }
 }
