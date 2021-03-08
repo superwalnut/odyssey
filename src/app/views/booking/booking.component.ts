b53bd7ea8855a1b690bc17a07a0f41c6148b47f8
vikingbei <bei2004@gmail.com>
2021-03-09 00:22:26 +1100

    collapse panel on booking UI

--- src/app/views/booking/booking.component.ts
+++ src/app/views/booking/booking.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingComponent implements OnInit {
 
+  panelOpenState = false;
+
+
   constructor() { }
 
   ngOnInit(): void {
