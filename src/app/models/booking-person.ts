6826649ece590921d6d565921a55d7717a0c584f
kevin <kevin.wang@ladbrokes.com.au>
2021-03-08 13:39:31 +1100

    booking tweaks

---
+++ src/app/models/booking-person.ts
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import Timestamp = firebase.firestore.Timestamp;
+
+export class BookingPerson {
+    userId:string;
+    userDisplayName:string;
+    isVIP:boolean;
+    notes:string; //Only admin can enter notes
+    paymentMethod:string; //Credit | Cash | Bank Transfer
+    
+    parentUserId:string; //payment deduct from this user if payment method is CREDIT
+    
+    amount:number;
+    
+    isPaid:boolean; //this usually apply to cash only
+    
+    createdOn:Timestamp;
+}
