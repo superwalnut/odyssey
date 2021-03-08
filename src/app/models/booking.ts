d5c7b17c5009de3a12c34838935cb0a5b733ac89
kevin <kevin.wang@ladbrokes.com.au>
2021-03-08 16:34:47 +1100

    add groupId to booking

--- src/app/models/booking.ts
+++ src/app/models/booking.ts
@@ -4,7 +4,8 @@ import Timestamp = firebase.firestore.Timestamp;
 
 export class Booking {
     docId:string;
-    sessionDate: Timestamp;
+    groupId:string;
+    date: Timestamp;
     weekDay:string; // TUE | FRI | SAT - make it easier for query
     people:BookingPerson[];
     isLocked:boolean // once locked, even admin can't make edit, only God can make changes to it
