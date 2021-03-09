17d6eb9db7147e74216c8b6f3306a1a70d9bb7bf
vikingbei <bei2004@gmail.com>
2021-03-09 19:28:56 +1100

    group UI big change

--- src/app/views/admin/bookings/bookings.component.ts
+++ src/app/views/admin/bookings/bookings.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Group } from "../../../models/group";
+import { GroupService } from "../../../services/group.service";
+import { AccountService } from "../../../services/account.service";
+import { group } from '@angular/animations';
+
 
 @Component({
   selector: 'app-bookings',
@@ -7,16 +12,38 @@ import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog
   styleUrls: ['./bookings.component.scss']
 })
 export class BookingsComponent implements OnInit {
+  myGroups = [];
+  myDocId: string;
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog, private groupService: GroupService, private accountService: AccountService) { }
 
   ngOnInit(): void {
-
+    this.myDocId = this.accountService.getLoginAccount().docId;
+    this.getMyGroups();
   }
+
   toggleLock(name: string) {
-    if(confirm("Comfirm to lock/unlock "+name)) {
+    if (confirm("Comfirm to lock/unlock " + name)) {
       console.log("Implement delete functionality here");
     }
+  }
+
+  getMyGroups() {
+    this.groupService.getGroupsByUserDocId(this.myDocId).subscribe(x => {
+
+      x.forEach(g => {
+        this.myGroups.push({ 'docId': g.docId, 'groupName': g.groupName });
+      })
+
+      console.log(this.myGroups)
+    });
+
+
+    // this.slides.push({
+    //   image: `https://picsum.photos/seed/${seed}/900/500`
+    // });
+
+
   }
 
 }
