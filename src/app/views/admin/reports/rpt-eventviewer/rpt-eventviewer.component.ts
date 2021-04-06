bc7f2ba95212d7399736e09e7b96e64ce72717d7
Luc <bei2004@gmail.com>
2021-04-06 17:14:30 +1000

    event viewer

--- src/app/views/admin/reports/rpt-eventviewer/rpt-eventviewer.component.ts
+++ src/app/views/admin/reports/rpt-eventviewer/rpt-eventviewer.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { BaseComponent } from '../../../base-component';
+import { EventLoggerService } from "../../../../services/event-logger.service";
+import { EventLogger } from "../../../../models/event-logger";
 
 @Component({
   selector: 'app-rpt-eventviewer',
   templateUrl: './rpt-eventviewer.component.html',
   styleUrls: ['./rpt-eventviewer.component.scss']
 })
-export class RptEventviewerComponent implements OnInit {
+export class RptEventviewerComponent extends BaseComponent implements OnInit {
 
-  constructor() { }
+  logs:EventLogger[];
+  constructor(private eventLogService:EventLoggerService) { super() }
 
   ngOnInit(): void {
+    this.getEventLogs();
+  }
+
+  getEventLogs() {
+    this.eventLogService.getAllEventLogs().subscribe(result=>{
+      this.logs = result;
+
+    })
+
   }
 
 }
