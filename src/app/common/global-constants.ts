8f1562326e43d171dd513f4b4e86236db7ef84c1
Luc <bei2004@gmail.com>
2021-04-06 19:08:09 +1000

    price change to $18

--- src/app/common/global-constants.ts
+++ src/app/common/global-constants.ts
@@ -36,7 +36,7 @@ export class GlobalConstants {
 
 
     //Price, Rates
-    public static rateCash: number = 17;
+    public static rateCash: number = 18;
     public static rateCredit: number = 15;
     public static rateFamily: number = 14; //1 person charge 15, the rest 14 each
 
