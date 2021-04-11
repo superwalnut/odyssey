2a5371938a9f3248c4936b0b78c75d692f3164c3
kevin <kevin.wang@ladbrokes.com.au>
2021-04-11 10:32:49 +1000

    setup dev

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,22 +7,13 @@ export const environment = {
   production: false,
   mail_url: "http://localhost:5001/hbclub-919aa/us-central1",
   firebase: {
-    apiKey: "AIzaSyDdRojW_gDRUM6rCoMApNu0Q585rU2GIe8",
-    authDomain: "hbclub-919aa.firebaseapp.com",
-    projectId: "hbclub-919aa",
-    storageBucket: "hbclub-919aa.appspot.com",
-    messagingSenderId: "662012824452",
-    appId: "1:662012824452:web:53cfe97159e39745a5abd1",
-    measurementId: "G-E3M8ZFWFZB",
-    mailgunapi:"520a91c49b5a694b32f640f22dd313af-29561299-804372a6"
-
-
-    // databaseURL: "https://hbc666.firebaseio.com",
-    // apiKey: "AIzaSyB6TJWbzv7NzP-qqAbF2-WUlbr6zPXn-8U",
-    // authDomain: "hbc666.firebaseapp.com",
-    // projectId: "hbc666",
-    // storageBucket: "hbc666.appspot.com",
-    // messagingSenderId: "518711263684",
-    // appId: "1:518711263684:web:0b78a2707b096b2831a491"
+    mailgunapi:"520a91c49b5a694b32f640f22dd313af-29561299-804372a6",
+    apiKey: "AIzaSyCV2fY_LZCFmFuAE7YaBe2vdrrCue3ca0U",
+    authDomain: "hbc-dev-c5967.firebaseapp.com",
+    projectId: "hbc-dev-c5967",
+    storageBucket: "hbc-dev-c5967.appspot.com",
+    messagingSenderId: "859665213873",
+    appId: "1:859665213873:web:dce5b954eddb327d843590",
+    measurementId: "G-F7GD08WM72"
   }
 };
