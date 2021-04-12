83ad620db0277a785e6ff6c2eb28c6e2cb7c0020
Luc <bei2004@gmail.com>
2021-04-12 14:25:04 +1000

    firebase UAT is not the new DEV
    pelase, delete dev app and use UAT instead

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,12 +8,22 @@ export const environment = {
   mail_url: "http://localhost:5001/hbclub-919aa/us-central1",
   firebase: {
     mailgunapi:"520a91c49b5a694b32f640f22dd313af-29561299-804372a6",
-    apiKey: "AIzaSyCV2fY_LZCFmFuAE7YaBe2vdrrCue3ca0U",
-    authDomain: "hbc-dev-c5967.firebaseapp.com",
-    projectId: "hbc-dev-c5967",
-    storageBucket: "hbc-dev-c5967.appspot.com",
-    messagingSenderId: "859665213873",
-    appId: "1:859665213873:web:dce5b954eddb327d843590",
-    measurementId: "G-F7GD08WM72"
+    apiKey: "AIzaSyARtsKbJSMmNK4gX-KVODBGc6JNociGU8M",
+    authDomain: "hbc-uat-9f2ad.firebaseapp.com",
+    projectId: "hbc-uat-9f2ad",
+    storageBucket: "hbc-uat-9f2ad.appspot.com",
+    messagingSenderId: "733704090264",
+    appId: "1:733704090264:web:9f62c53e02f071b92716ed",
+    measurementId: "G-DC4S7YNP37"
   }
+  // firebase: {
+  //   mailgunapi:"520a91c49b5a694b32f640f22dd313af-29561299-804372a6",
+  //   apiKey: "AIzaSyCV2fY_LZCFmFuAE7YaBe2vdrrCue3ca0U",
+  //   authDomain: "hbc-dev-c5967.firebaseapp.com",
+  //   projectId: "hbc-dev-c5967",
+  //   storageBucket: "hbc-dev-c5967.appspot.com",
+  //   messagingSenderId: "859665213873",
+  //   appId: "1:859665213873:web:dce5b954eddb327d843590",
+  //   measurementId: "G-F7GD08WM72"
+  // }
 };
