db6aa6caaff7d18456df1de87bab083efdd0dd4d
Kevin Wang (Contractor) <wwang1@woolworths.com.au>
2022-06-01 14:45:00 +1000

    remove secrets

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,15 @@
 export const environment = {
   env: 'prod',
   production: true,
-  mail_url: "https://us-central1-hbclub-919aa.cloudfunctions.net",
+  mail_url: "",
   firebase: {
-    apiKey: "AIzaSyDdRojW_gDRUM6rCoMApNu0Q585rU2GIe8",
-    authDomain: "hbclub-919aa.firebaseapp.com",
-    projectId: "hbclub-919aa",
-    storageBucket: "hbclub-919aa.appspot.com",
-    messagingSenderId: "662012824452",
-    appId: "1:662012824452:web:53cfe97159e39745a5abd1",
-    measurementId: "G-E3M8ZFWFZB",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
   },
   domain: "https://hbc666.club",
 };
--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -1,15 +1,15 @@
 export const environment = {
     env: 'uat',
     production: false,
-    mail_url: "https://us-central1-hbclub-919aa.cloudfunctions.net",
+    mail_url: "",
     firebase: {
-      apiKey: "AIzaSyARtsKbJSMmNK4gX-KVODBGc6JNociGU8M",
-      authDomain: "hbc-uat-9f2ad.firebaseapp.com",
-      projectId: "hbc-uat-9f2ad",
-      storageBucket: "hbc-uat-9f2ad.appspot.com",
-      messagingSenderId: "733704090264",
-      appId: "1:733704090264:web:9f62c53e02f071b92716ed",
-      measurementId: "G-DC4S7YNP37"
+      apiKey: "",
+      authDomain: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: "",
+      measurementId: ""
     },
     domain: "https://hbc-uat-9f2ad.web.app",
   };
