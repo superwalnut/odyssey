fca29e1fb1e6e791ad8aa1b50a7715f2a145a31f
Kevin Wang (Contractor) <wwang1@woolworths.com.au>
2022-06-03 14:34:48 +1000

    undo delete

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  env: 'prod',
+  production: true,
+  mail_url: "",
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
+  },
+  domain: "https://hbc666.club",
+};
