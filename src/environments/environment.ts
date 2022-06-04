7fc652f83d652721949f5db344d2f9420bdd3dea
Kevin Wang (Contractor) <wwang1@woolworths.com.au>
2022-06-04 20:38:54 +1000

    remove local env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,16 @@
-import {firebase} from './local.environment'
 
 export const environment = {
   env: 'local',
   production: 'false',
   mail_url: 'http://localhost:5001/hbclub-919aa/us-central1',
   domain: "https://hbc666.club",
-  ...firebase
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
+  },
 };
