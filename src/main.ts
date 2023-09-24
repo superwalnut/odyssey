91045619f7f8d9a72230ca3dac138a309d487e4f
Kevin Wang (Contractor) <wwang1@woolworths.com.au>
2023-09-25 09:56:41 +1000

    remove log

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
   if (window) {
-    //window.console.log = function() {}
+    window.console.log = function() {}
   }
 }
 
